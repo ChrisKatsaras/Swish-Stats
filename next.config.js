39ea6d98d459cef927217445cbbdb269f313e0bd
Christopher Katsaras <chriskatsaras5@gmail.com>
2019-03-28 22:45:56 -0400

    Implemented Webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
-const withCSS = require('@zeit/next-css')
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS({
-  cssLoaderOptions: {
-    url: false
-  }
-})
+const nextConfig = {
+  useFileSystemPublicRoutes: false,
+  distDir: 'out',
+};
+
+module.exports = withPlugins([
+  [withCSS, {
+    cssloaderOptions: {
+      url: false
+    }
+  }],
+  [withBundleAnalyzer, {
+    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../../bundles/server.html'
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html'
+      }
+    }
+  }]
+], nextConfig);
