b37d937515dcceb14400075752f87db207467e36
Christopher Katsaras <chriskatsaras5@gmail.com>
2019-04-06 22:18:35 -0400

    Typeahead now queries balldontlie API for players via first / last name

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout.js'
 import fetch from 'isomorphic-unfetch'
 import {AsyncTypeahead} from 'react-bootstrap-typeahead';
+import { debounce } from 'throttle-debounce';
 
 const masthead = {
     background: 'linear-gradient(#16222A, #343a44)',
@@ -12,26 +13,67 @@ const fontPrimary = {
     fontWeight: 'bold'
 }
 
-const Index = (props) => (
+export default class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false,
+            options: []
+          };
+        this._handleSearch = this._handleSearch.bind(this);
+    }
     
-    <Layout>
-        <div style={masthead} className="position-relative overflow-hidden text-center">
-            <div className="col-md-5 p-lg-5 mx-auto my-5">
-                <h1 className="display-4 text-light" style={fontPrimary}>Swish Stats</h1>
-            </div>
+    componentDidMount() {
+        this.setState({isLoading:false})
+    }
 
-            <AsyncTypeahead
-                onSearch={query => {
-                    fetch(`https://api.github.com/search/users?q=${query}`)
-                    
-                }}
-                className="col-md-5 p-lg-5 mx-auto my-5"/>
+    _handleSearch = debounce(1000, function(query) {
+        console.log(query)
+        this.setState({isLoading: true});
+        fetch(`https://www.balldontlie.io/api/v1/players?search=${query}`)
+        .then(resp => resp.json())
+        .then(json => this.setState({
+            isLoading: false,
+            options: json.data,
+        }));
+    });
 
-            <div className="product-device shadow-sm d-none d-md-block"></div>
-            <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-        </div>
+    render() {
+        const filterByFields = ['first_name', 'last_name'];
+        return (
+            <Layout>
+                <div style={masthead} className="position-relative overflow-hidden text-center">
+                    <div className="col-md-5 p-lg-5 mx-auto my-5">
+                        <h1 className="display-4 text-light" style={fontPrimary}>Swish Stats</h1>
+                    </div>
 
-    </Layout>
-)
-  
-export default Index
+                    <AsyncTypeahead
+                        id="Search"
+
+                        {...this.state}
+                        filterBy={ filterByFields}
+                        labelKey={(option) => `${option.first_name}`}
+                        isLoading={this.state.isLoading}
+                        minLength={3}
+                        name="CompanyId"
+                        onSearch={this._handleSearch}
+                        placeholder="Search Player Name"
+                        className="col-md-5 p-lg-5 mx-auto my-5"
+                        options={this.state.options}
+                        renderMenuItemChildren={(option) => (
+                            <div>
+                                {option.first_name} {option.last_name}
+                                <div>
+                                    <small>Team: {option.team.full_name}</small>
+                                </div>
+                            </div>
+                        )}
+                    />
+
+                    <div className="product-device shadow-sm d-none d-md-block"></div>
+                    <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+                </div>
+            </Layout>
+        )
+    }
+}
